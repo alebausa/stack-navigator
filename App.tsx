ac0510a48458bbb5632b7ef0ffb9bc43b4748f8c
Ale Bausa <ale.bausa@gmail.com>
2023-07-18 17:34:20 +0200

    adds packages

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <NavigationContainer>
+      <View>
+        <Text>App</Text>
+      </View>
+    </NavigationContainer>
   )
 }
 
